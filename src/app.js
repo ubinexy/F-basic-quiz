45eca75e9684179fd4ddde6341ae9bc59fa2f29c
Qinyun Wang <qinyun.wang@rea-group.com>
2020-06-19 21:29:23 +0800

    Add fetch function to get user name from backend

--- src/app.js
+++ src/app.js
@@ -5,10 +5,13 @@ const baseUrl = "http://localhost:8080/users/";
 const getName = async (userId) => {
   return fetch(`${baseUrl}${userId}`, {
     headers: {
-      'content-type': 'application/json'
+      "content-type": "application/json",
+      "Access-Control-Allow-Origin": "true",
     },
-    method: 'GET'
-  })
-}
+    method: "GET",
+  });
+};
 
-$('#name').html(getName("1"));
+document.getElementById("name").innerHTML = getName("1").then((res) =>
+  res.json()
+);
