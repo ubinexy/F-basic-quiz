37534d265a3f32cff62b300ec37d23e887cc0cfa
杜娟 <jdu.xian@gmail.com>
2020-06-19 19:01:33 +0800

    update README

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
 import "./style/index.scss";
+
+const baseUrl = "http://localhost:8080/users/";
+
+const getName = async (userId) => {
+  return fetch(`${baseUrl}${userId}`, {
+    headers: {
+      'content-type': 'application/json'
+    },
+    method: 'GET'
+  })
+}
+
+$('#name').html(getName("1"));
