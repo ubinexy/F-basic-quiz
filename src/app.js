119ce3022547c26b6f6ffabfbb05367f93e6447d
shiqi <shiqi.tong@thoughtworks.com>
2021-01-31 22:20:10 +0800

    [feat][shiqi]: render user header

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 import "./style/index.scss";
-
-const baseUrl = "http://localhost:8080/users/";
-
-const getName = async (userId) => {
-  return fetch(`${baseUrl}${userId}`, {
-    headers: {
-      'content-type': 'application/json'
-    },
-    method: 'GET'
-  })
-}
-
-$('#name').html(getName("1"));
+// const baseUrl = "http://localhost:3000/users/";
+//
+// const getName = async (userId) => {
+//   return fetch(`${baseUrl}${userId}`, {
+//     headers: {
+//       'content-type': 'application/json'
+//     },
+//     method: 'GET'
+//   })
+// }
+//
